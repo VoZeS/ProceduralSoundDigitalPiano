2d232ca5e0ad41a5622b25654996d33d393b8155
VoZeS <79161189+VoZeS@users.noreply.github.com>
2024-06-11 18:11:07 +0200

    small changes

--- ProceduralPiano/Assets/Scenes/Scripts/AudioCode/SquareWave.cs
+++ ProceduralPiano/Assets/Scenes/Scripts/AudioCode/SquareWave.cs
@@ -1,21 +1,14 @@
-﻿/*	Author: Kostas Sfikas
-	Date: April 2017
-	Language: c#
-	Platform: Unity 5.5.0 f3 (personal edition) */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class SquareWave{
-	/* This class creates an approximation of a Square Wave, by using harmonics of a sinus wave.
-	This way the actual shape of the wave is smoother than an absolute square shape, thus reducing
-	some of the noise this would create.
-	The levels of each harmonic are stored in the squareHarmonicLevels Array */
-	double[] squareHarmonicLevels = new double[] 
-	{
-		1.0, 		// level of 1st harmonic
+public class SquareWave
+{
+
+    double[] squareHarmonicLevels = new double[]
+    {
+        1.0, 		// level of 1st harmonic
 		0.0,		// level of 2nd harmonic
 		0.33333333,	// level of 3rd harmonic
 		0.0,		// level of 4th harmonic
@@ -33,20 +26,24 @@ public class SquareWave{
 		0.0			// level of 16th harmonic
 	};
 
-	SinusWave[] subSignals;
+    SinusWave[] subSignals;
 
-	public SquareWave(){
-		subSignals = new SinusWave[16];
-		for (int i = 0; i < 16; i++) {
-			subSignals [i] = new SinusWave ();
-		}
-	}
+    public SquareWave()
+    {
+        subSignals = new SinusWave[16];
+        for (int i = 0; i < 16; i++)
+        {
+            subSignals[i] = new SinusWave();
+        }
+    }
 
-	public double calculateSignalValue(double newSignalTime, double newSignalFrequency){
-		double signalSum = 0.0;
-		for (int i = 0; i < subSignals.Length; i++) {
-			signalSum += squareHarmonicLevels[i] * subSignals [i].CalculateSignalValue (newSignalTime, newSignalFrequency * (i+1));
-		}
-		return signalSum;
-	}
+    public double calculateSignalValue(double newSignalTime, double newSignalFrequency)
+    {
+        double signalSum = 0.0;
+        for (int i = 0; i < subSignals.Length; i++)
+        {
+            signalSum += squareHarmonicLevels[i] * subSignals[i].CalculateSignalValue(newSignalTime, newSignalFrequency * (i + 1));
+        }
+        return signalSum;
+    }
 }
--- ProceduralPiano/Assets/Scenes/Scripts/RecordCode/RecordMainCode.cs
+++ ProceduralPiano/Assets/Scenes/Scripts/RecordCode/RecordMainCode.cs
@@ -73,9 +73,12 @@ public class OutputAudioRecorder : MonoBehaviour
     {
         var intData = new Int16[dataSource.Length];
         //converting in 2 steps : float[] to Int16[], //then Int16[] to Byte[]
+
         var bytesData = new Byte[dataSource.Length * 2];
+
         //bytesData array is twice the size of
         //dataSource array because a float converted in Int16 is 2 bytes.
+
         var rescaleFactor = 32767; //to convert float to Int16
         for (var i = 0; i < dataSource.Length; i++)
         {
